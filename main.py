b9a1a256b6dafad32d542614359e7cdaf846ea72
krisztianbedo <bedo.krisztian@gmail.com>
2021-11-28 12:07:57 +0100

    first commit

--- main.py
+++ main.py
@@ -3,17 +3,8 @@ import logging
 from confluent_kafka import Producer
 import socket
 
-conf = {'bootstrap.servers': "localhost:9092",
-        'client.id': socket.gethostname()}
-producer = Producer(conf)
-topic = 'alma'
+from confluent_kafka import Producer
 
-logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
-logging.info(f'process started')
-
-for i in range(0,100):
-    logging.info(f'processing... ({i})')
-    time.sleep(1)
-    producer.produce(topic, key="key", value="value")
-
-logging.info(f'process ended')
+p = Producer({'bootstrap.servers': 'localhost:9091'})
+p.produce('light_bulb', key='hello', value='world werwr')
+p.flush(30)
